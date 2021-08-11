5449dde6211c6870f47dad05de102647aa17bbb3
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-12 00:18:37 +0300

    Degiskenler

---
+++ Değişkenler/Variables1.py
@@ -0,0 +1,43 @@
+sayi1 = 10
+sayi2 = 20
+
+# 3sayi => yanlis. Degiskenler sayi ile baslayamaz.
+
+_sayi3 = 30
+# sayi@ = 40 # yanlis
+
+sayi_4 = 50 # degiskenler bosluk iceremez.
+
+sayı5 = 50 # turkce karakter icermemelidir.
+
+yas = 20 # buyuk harf duyarliligi vardir.
+YAS = 30
+
+print(yas)
+print(YAS)
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+
+x = 1
+print(type(x))
+
+y = 1.5
+print(type(y))
+
+name = "Smtrbci"
+print(type(name))
+
+isStudent = True
+isStudent = False
+
+print(type(isStudent))
+
+x, y, name, isStudent = 1, 1.5, "Smtrbci", False
+
+a = '10'
+
+b = '20'
+
+toplam = a + b
+print(toplam)
