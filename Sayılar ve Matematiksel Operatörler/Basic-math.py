bdc669174f413fe52296adb972f0c0c8d693a068
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-11 21:21:57 +0300

    Sayilar ve Matematiksel Operatorler

---
+++ Sayılar ve Matematiksel Operatörler/Basic-math.py
@@ -0,0 +1,53 @@
+# print(3+4)
+
+# print(3.0+4)
+
+# print(3.2+4)
+
+# print(type(3.0+4))
+
+# print(3*3)
+
+# print(3.0*3)
+
+# print(10/3)
+
+# print(10/5)
+
+# print(type(10/5))
+
+# print(3+5*2)
+
+# print((3+5)*2)
+
+# print(3+5*6/3)
+
+# print((3+5)*6/2)
+
+# print(2**3)
+
+# print(3**2)
+
+# print(25**0.5)
+
+# print(10%3)
+
+# print(10%4)
+
+# print(100%2)
+
+# print(101%2)
+
+# print(1000000001&2)
+
+# print(10/3)
+
+# print(10//3)
+
+# print(93//9)
+
+# print((12//5)**2)
+
+# print((12//5)**2*7)
+
+# print((12//5)**2)*7)
---
+++ Sayılar ve Matematiksel Operatörler/comment.py
@@ -0,0 +1,9 @@
+# // (tam bolen): 10 sayisini 3 bolunur ve 
+# tam kismi alinir.
+
+"""
+# // (tam bolen): 10 sayisini 3 bolunur ve 
+# tam kismi alinir.
+"""
+
+print(10//3)
