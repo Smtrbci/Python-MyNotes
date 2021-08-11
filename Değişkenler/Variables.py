acec98caad716e4827bb6eb46f98a1a3cd143339
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-11 23:59:13 +0300

    Degiskenler

---
+++ Değişkenler/Variables.py
@@ -0,0 +1,14 @@
+# 5000 + %18
+
+# print(5000 + (5000 * 0.18))
+# print(6000 + (6000 * 0.18))
+
+urunA = 5000
+urunB = 6000
+urunC = 7000
+
+kdv = 0.18
+
+print(urunA + (urunA * kdv))
+print(urunB + (urunB * kdv))
+print(urunC + (urunC * kdv))
