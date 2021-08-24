721663faa345345b8cb44a1106326c8c98a115f2
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-08-24 21:06:27 +0300

    Değişkenler

---
+++ Değişkenler/data-types.py
@@ -0,0 +1,59 @@
+'''
+# int, Float, String, bool
+
+# x = 10
+# y = 20
+
+# x = input("x: ")
+# y = input("y: ")
+
+# x = int("20")
+# y = int("10")
+
+print(type(x))
+print(type(y))
+
+toplam = x + y
+print(toplam)
+'''
+
+age = 5
+weight = 2.5
+name = 'Github'
+isStudent = True
+
+"""
+print(type(age))
+print(type(weight))
+print(type(name))
+print(type(isStudent))
+"""
+
+# int to float
+result = float(age)
+
+print(result)
+print(type(result))
+
+
+# float to int
+result = int(weight)
+
+print(result)
+print(type(result))
+
+
+# bool to str 
+result = str(result)
+
+print(result)
+print(type(result))
+
+# isStudent = False
+# print(isStudent)
+
+# bool to int
+
+result = int(isStudent)
+print(result)
+print(type(result))
