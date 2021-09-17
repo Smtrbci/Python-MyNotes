51f48333a48caa3a0a4118a433929c64a34c16eb
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-09-17 21:59:51 +0300

    Strings Karakter Dizileri

---
+++ Strings Karakter Dizileri/string-metotlari.py
@@ -0,0 +1,29 @@
+msg = "Welcome to Github"
+
+# sonuc = msg.upper()
+# sonuc = msg.lower()
+# sonuc = msg.title()
+# sonuc = msg.capitalize()
+
+# sonuc = "abc".islower()
+
+# sonuc = "   abc ".strip()
+# sonuc = msg.split()
+
+# print(sonuc[0])
+
+# sonuc = msg.split('.')
+
+# sonuc = "-".join(sonuc)
+
+# index = msg.index('Welcome')
+# print(index)
+
+# sonuc = msg.startswith('A') 
+
+# sonuc = msg.endswith('b')
+
+sonuc = msg.replace('to','Github')
+sonuc = msg.replace(' ','-')
+
+print(sonuc)
