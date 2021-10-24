b7f3140524cab816dd2502e9fc004ff658f7570e
Smtrbci <75490736+Smtrbci@users.noreply.github.com>
2021-10-24 23:32:35 +0300

    Listeler

---
+++ Listeler/lists.py
@@ -0,0 +1,25 @@
+# msg = 'Welcome to Github'
+msg = "Welcome to Github".split()
+
+sayilar = [1,3,5,7,9]
+
+# sonuc = sayilar
+sonuc = sayilar[0]
+sonuc = sayilar[4]
+# sonuc = sayilar[5] # IndexError: list index out of range
+
+isimler = ['Mustafa','Ali','Ada','Ceyda']
+
+sonuc = isimler
+sonuc = isimler[0]
+
+print(type(isimler[0]))
+
+listeAli = ['ali',20]
+listeAhmet = ['ahmet',22]
+
+sonuc = listeAli[0]
+sonuc = listeAli[1]
+
+
+print(sonuc)
